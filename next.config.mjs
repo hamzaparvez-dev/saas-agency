b48564c9c0843fa02f252427d03a2dad11a33140
hamza <hamza@hamzas-MacBook-Air.local>
2025-09-17 13:50:46 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      { // Add this new block
+        protocol: 'https',
+        hostname: 'cdn.example.com',
+      },
     ],
   },
 };
